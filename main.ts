da7be0747ee7c41d149e93d140dddcade177d241
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-12-18 13:25:36 +0900

    a bit more

--- main.ts
+++ main.ts
@@ -1,18 +1,21 @@
 import { parse } from "npm:@plussub/srt-vtt-parser";
-import { readFileSync } from "node:fs";
+import { readFileSync, createWritableStream } from "node:fs";
 
 /**
  * Extract text from subtitles file at `path`.
  * Paragraphs (detected as long pauses) are split in the output.
  */
-function processFile(path: string) {
+function* processFile(path: string) {
   const str = readFileSync(path, { encoding: "utf-8" });
   let prevEnd = 0;
-  parse(str).forEach(({ from: start, to: end, text }) => {
+  for (const { from: start, to: end, text } of parse(str)) {
     if (start - prevEnd > 1000) {
-      flush("\n");
+      yield "\n";
     }
-    flush(text);
+    yield text;
+    yield "\n";
     prevEnd = end;
-  });
+  }
 }
+
+// TODO: command line, output, run on folder
